f22779c789bb81d63e762598e35617de07890882
Wei Lin Li <wlin.li2403@gmail.com>
2021-09-19 20:25:56 -0400

    updated cout lines

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 // add your code as appropriate 
 #include <iostream>
-
+#include "reservoir.h"
 
 int main()
 {
-  double basinStorage = get_east_storage(05/20/2018);
-  std::cout << basinStorage << endl;
+  //tests for getting east basin storage
+  //test one
+  std::cout << "Enter date: 05/20/2018\n" << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons" << std::endl;
+  //test two
+  std::cout << "Enter date: 03/12/2018\n" << "East basin storage: " << get_east_storage("03/12/2018") << " billion gallons" << std::endl;
+
+  
 
   
 
